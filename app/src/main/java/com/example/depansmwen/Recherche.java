92c8d180d9439b5467efbb7420f9ebdcc8e26647
Ing. Pehguy <fpehguy@gmail.com>
2019-10-05 11:36:39 -0700

    afficher les informations de depense(without create compte)

--- app/src/main/java/com/example/depansmwen/Recherche.java
+++ app/src/main/java/com/example/depansmwen/Recherche.java
@@ -2,13 +2,15 @@ package com.example.depansmwen;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Recherche extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.recherche);
+        Button btn = findViewById(R.id.buttonoui);
     }
 }
